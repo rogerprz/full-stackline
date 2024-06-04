7ba0b96561b02244b10b816d3f140a8667e8d275
Roger Perez <ropere@microsoft.com>
2024-06-04 10:29:55 -0700

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'full-stackline',
+  base: '/full-stackline/',
   server: {
     open: true
   }
